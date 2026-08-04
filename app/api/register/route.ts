708d6f21d5bede6a21406d654746e8fa9c7c4d43
nongyu88 <john.kidsman@gmail.com>
2026-08-05 01:36:08 +0800

    Implemented: business, light mode

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { NextResponse } from "next/server";
 import { prisma } from "../../../lib/prisma"; // Adjust path if needed
 import bcrypt from "bcryptjs";
